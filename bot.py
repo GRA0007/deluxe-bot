0f5421770a508937923fe501dd69dc0dd622727b
nullableVoidPtr <30564701+nullableVoidPtr@users.noreply.github.com>
2020-02-07 18:54:40 +0800

    Remove need for temp.png, delegate bot channels to server owner, use CairoSVG

--- bot.py
+++ bot.py
@@ -2,8 +2,7 @@ import discord
 import os
 
 from bs4 import BeautifulSoup
-import wand.color
-import wand.image
+from cairosvg import svg2png
 from PIL import Image
 import requests
 from io import BytesIO
@@ -83,12 +82,13 @@ rank_colors = {
 
 # Draw a profile card
 def draw_image(profile):
-    svg_code = None
     with open('template.svg', 'r', encoding="utf8") as f:
         data_text = f.readlines()
         svg_code = ' '.join(data_text)
 
-    if svg_code is not None:
+        if svg_code is None:
+            return(None)
+
         # Edit SVG
         soup = BeautifulSoup(svg_code, 'xml')
 
@@ -129,31 +129,28 @@ def draw_image(profile):
         # Convert SVG to PNG
         svg_blob = str.encode(str(soup))
 
-        with wand.image.Image(blob=svg_blob, format="svg") as image:
-            png_image = image.make_blob("png")
+        with BytesIO() as png_image:
+            svg2png(bytestring=svg_blob, write_to=png_image)
+            png_image.seek(0)
+            pil_img = Image.open(png_image)
 
-        img_buffer = numpy.asarray(bytearray(png_image), dtype='uint8')
-        bytesio = BytesIO(img_buffer)
-        pil_img = Image.open(bytesio)
+            # Profile image
+            response = requests.get(profile['image'])
+            profile_image = Image.open(BytesIO(response.content))
+            profile_image = profile_image.resize((220, 220), Image.ANTIALIAS)
 
-        # Profile image
-        response = requests.get(profile['image'])
-        profile_image = Image.open(BytesIO(response.content))
-        profile_image = profile_image.resize((220, 220), Image.ANTIALIAS)
+            # Grade image
+            response = requests.get(profile['grade'])
+            grade_image = Image.open(BytesIO(response.content))
+            grade_image = grade_image.resize((95, 48), Image.ANTIALIAS)
 
-        # Grade image
-        response = requests.get(profile['grade'])
-        grade_image = Image.open(BytesIO(response.content))
-        grade_image = grade_image.resize((95, 48), Image.ANTIALIAS)
+            pil_img.paste(profile_image, (40, 40), profile_image)
+            pil_img.paste(grade_image, (280, 210), grade_image)
 
-        pil_img.paste(profile_image, (40, 40), profile_image)
-        pil_img.paste(grade_image, (280, 210), grade_image)
-
-        image_name = 'temp.png'
-        pil_img.save(image_name)
-        return(image_name)
-    else:
-        return(None)
+            image = BytesIO()
+            pil_img.save(image, 'PNG')
+            image.seek(0)
+            return(image)
 
 
 class Dxbot(discord.Client):
@@ -168,40 +165,34 @@ class Dxbot(discord.Client):
 
         input = message.content.strip()
         try:
-            # If input is a number and channel is Benpai's testing channel,
-            # or #dx-friend-codes channel
-            if int(input) and (message.channel.id == 352449754573045763 or
-                               message.channel.id == 670567133700161568):
+            await message.channel.trigger_typing()
+            intl = scraper.get_user(input, False)
+            jp = None
+            if intl == None:
                 await message.channel.trigger_typing()
-                intl = scraper.get_user(input, False)
-                jp = None
-                if intl is None:
-                    await message.channel.trigger_typing()
-                    jp = scraper.get_user(input, True)
+                jp = scraper.get_user(input, True)
 
-                if intl is None and jp is None:
-                    await message.channel.send('Profile not found')
-                else:
-                    type = ''
-                    friend_url = ''
-                    profile = None
-                    if intl is not None:
-                        profile = intl
-                        type = 'International'
-                        friend_url = 'https://maimaidx-eng.com/maimai-mobile/friend/search/searchUser/?friendCode=' + input
-                    elif jp is not None:
-                        profile = jp
-                        type = 'Japanese'
-                        friend_url = 'https://maimaidx.jp/maimai-mobile/friend/search/searchUser/?friendCode=' + input
+            if intl == None and jp == None:
+                await message.channel.send('Profile not found')
+            else:
+                type = ''
+                friend_url = ''
+                profile = None
+                if intl != None:
+                    profile = intl
+                    type = 'International'
+                    friend_url = 'https://maimaidx-eng.com/maimai-mobile/friend/search/searchUser/?friendCode=' + input
+                elif jp != None:
+                    profile = jp
+                    type = 'Japanese'
+                    friend_url = 'https://maimaidx.jp/maimai-mobile/friend/search/searchUser/?friendCode=' + input
 
-                    image = draw_image(profile)
+                image = draw_image(profile)
 
-                    file = discord.File(image, filename="profile.png")
-                    embed = discord.Embed(color=0x51bcf3,
-                                          title=type + ' profile for ' + profile['name'],
-                                          url=friend_url)
-                    embed.set_image(url="attachment://profile.png")
-                    await message.channel.send(file=file, embed=embed)
+                file = discord.File(image, filename="profile.png")
+                embed = discord.Embed(color=0x51bcf3, title=type + ' profile for ' + profile['name'], url=friend_url)
+                embed.set_image(url="attachment://profile.png")
+                await message.channel.send(file=file, embed=embed)
         except ValueError:
             return
 
